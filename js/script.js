57402a7c88645710ca3bf187d9f899a9aaa1c7d7
kamenokosystem <e115725@ie.u-ryukyu.ac.jp>
2022-10-12 20:35:51 +0900

    プロフ画像の変更

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 $(function () {
 
+  //ナビゲーションバーがトップで固定された際に影をつける
+  const select = document.querySelector('.gnav-dummy');
+  const observer = new window.IntersectionObserver( (entry) => {
+  if (!entry[0].isIntersecting) {
+      document.querySelector('.gnav').classList.add('gnav-shaddoow');
+    } else {
+      document.querySelector('.gnav').classList.remove('gnav-shaddoow');
+    }
+  });
+  observer.observe(select);
+
+  //トップロゴのアニメーション
+  const div = document.querySelector('.logo-img')
+  div.animate([{opacity: '0'}, {opacity: '1'}], 1500);
+
   //ページ内スクロール
   var navHeight = $(".gnav").outerHeight();
 
@@ -7,13 +22,13 @@ $(function () {
     var href = $(this).attr("href");
     var target = $(href == "#" || href == "" ? "html" : href);
     var position = target.offset().top - navHeight;
-    $("html, body").animate({ scrollTop: position, }, 300, "swing");
+    $("html, body").animate({ scrollTop: position, }, 900, "swing");
     return false;
   });
 
   //ページトップ
   $("#js-page-top").on("click", function () {
-    $("body,html").animate({ scrollTop: 0, }, 300);
+    $("body,html").animate({ scrollTop: 0, }, 900);
     return false;
   });
 
